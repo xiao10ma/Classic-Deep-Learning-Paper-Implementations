1af14325db05d94d53250fc58757e812d4322f23
xiao10ma <mazp@mail2.sysu.edu.cn>
2024-10-16 19:54:27 +0800

    UNet for DDPM

---
+++ UNet/DDPM_UNet.py/UNet.py
@@ -0,0 +1,104 @@
+import math
+import torch
+from torch import nn
+
+class Swish:
+    def forward(self, x):
+        return x * torch.sigmoid(x)
+    
+class TimeEmbedding(nn.Module):
+    """
+    ### Embeddings for $t$
+    """
+
+    def __init__(self, n_channels: int):
+        """
+        * `n_channels` is the number of dimensions in the embedding
+        """
+        super().__init__()
+        self.n_channels = n_channels
+        # First linear layer
+        self.lin1 = nn.Linear(self.n_channels // 4, self.n_channels)
+        # Activation
+        self.act = Swish()
+        # Second linear layer
+        self.lin2 = nn.Linear(self.n_channels, self.n_channels)
+
+    def forward(self, t: torch.Tensor):
+        # Create sinusoidal position embeddings
+        # [same as those from the transformer](../../transformers/positional_encoding.html)
+        #
+        # \begin{align}
+        # PE^{(1)}_{t,i} &= sin\Bigg(\frac{t}{10000^{\frac{i}{d - 1}}}\Bigg) \\
+        # PE^{(2)}_{t,i} &= cos\Bigg(\frac{t}{10000^{\frac{i}{d - 1}}}\Bigg)
+        # \end{align}
+        #
+        # where $d$ is `half_dim`
+        half_dim = self.n_channels // 8
+        emb = math.log(10_000) / (half_dim - 1)
+        emb = torch.exp(torch.arange(half_dim, device=t.device) * -emb)
+        emb = t[:, None] * emb[None, :]
+        emb = torch.cat((emb.sin(), emb.cos()), dim=1)
+
+        # Transform with the MLP
+        emb = self.act(self.lin1(emb))
+        emb = self.lin2(emb)
+
+        #
+        return emb
+    
+class ResidualBlock(nn.Module):
+    """
+    ### Residual block
+
+    A residual block has two convolution layers with group normalization.
+    Each resolution is processed with two residual blocks.
+    """
+
+    def __init__(self, in_channels: int, out_channels: int, time_channels: int,
+                 n_groups: int = 32, dropout: float = 0.1):
+        """
+        * `in_channels` is the number of input channels
+        * `out_channels` is the number of input channels
+        * `time_channels` is the number channels in the time step ($t$) embeddings
+        * `n_groups` is the number of groups for [group normalization](../../normalization/group_norm/index.html)
+        * `dropout` is the dropout rate
+        """
+        super().__init__()
+        # Group normalization and the first convolution layer
+        self.norm1 = nn.GroupNorm(n_groups, in_channels)
+        self.act1 = Swish()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=(3, 3), padding=(1, 1))
+
+        # Group normalization and the second convolution layer
+        self.norm2 = nn.GroupNorm(n_groups, out_channels)
+        self.act2 = Swish()
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=(3, 3), padding=(1, 1))
+
+        # If the number of input channels is not equal to the number of output channels we have to
+        # project the shortcut connection
+        if in_channels != out_channels:
+            self.shortcut = nn.Conv2d(in_channels, out_channels, kernel_size=(1, 1))
+        else:
+            self.shortcut = nn.Identity()
+
+        # Linear layer for time embeddings
+        self.time_emb = nn.Linear(time_channels, out_channels)
+        self.time_act = Swish()
+
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x: torch.Tensor, t: torch.Tensor):
+        """
+        * `x` has shape `[batch_size, in_channels, height, width]`
+        * `t` has shape `[batch_size, time_channels]`
+        """
+        # First convolution layer
+        h = self.conv1(self.act1(self.norm1(x)))
+        # Add time embeddings
+        h += self.time_emb(self.time_act(t))[:, :, None, None]
+        # Second convolution layer
+        h = self.conv2(self.dropout(self.act2(self.norm2(h))))
+
+        # Add the shortcut connection and return
+        return h + self.shortcut(x)
